a18baeaa00cf1afc02cf3030217a997167bdae98
Thomas Berghuis <thomas.berghuis@gmail.com>
2021-12-10 09:04:14 +1100

    .

--- app/src/main/java/xyz/tberghuis/surfcheckcompose/tmp/CamArray.kt
+++ app/src/main/java/xyz/tberghuis/surfcheckcompose/tmp/CamArray.kt
@@ -8,5 +8,9 @@ data class Cam(
 
 val camArray = arrayOf<Cam>(
   Cam("the wreck", "https://cams.cdn-surfline.com/cdn-au/au-byronbay/playlist.m3u8"),
-  Cam("the pass", "https://cams.cdn-surfline.com/cdn-au/au-thepassoverview/playlist.m3u8")
+  Cam("the pass", "https://cams.cdn-surfline.com/cdn-au/au-thepassoverview/playlist.m3u8"),
+  Cam("lennox", "https://cams.cdn-surfline.com/cdn-au/au-lennoxhead/playlist.m3u8"),
+  Cam("shelly", "https://cams.cdn-surfline.com/cdn-au/au-ballinashellybeach/playlist.m3u8"),
+  Cam("northwall", "https://cams.cdn-surfline.com/cdn-au/au-ballinalighthouse/playlist.m3u8"),
+  Cam("evans", "https://cams.cdn-surfline.com/cdn-au/au-evanshead/playlist.m3u8"),
 )
--- app/src/main/java/xyz/tberghuis/surfcheckcompose/tmp/NavDrawerDemo.kt
+++ app/src/main/java/xyz/tberghuis/surfcheckcompose/tmp/NavDrawerDemo.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
@@ -12,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -58,17 +61,26 @@ fun DrawerContent(scaffoldState: ScaffoldState, scope: CoroutineScope) {
   val playerViewModel = viewModel<PlayerViewModel>()
 
   Column(
-    modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
+    modifier = Modifier
+      .fillMaxWidth()
+      .verticalScroll(rememberScrollState()),
+    horizontalAlignment = Alignment.CenterHorizontally
   ) {
 //    Text("the wreck")
     Spacer(modifier = Modifier.padding(20.dp))
     camArray.forEachIndexed { i, cam ->
-      Text(cam.name, modifier = Modifier.clickable {
-        playerViewModel.currentCamIndexState.value = i
-        scope.launch {
-          scaffoldState.drawerState.close()
-        }
-      })
+      Text(
+        cam.name, modifier = Modifier
+          .padding(10.dp)
+          .clickable {
+            playerViewModel.currentCamIndexState.value = i
+            scope.launch {
+              scaffoldState.drawerState.close()
+            }
+          },
+        fontSize = 20.sp
+
+      )
       Spacer(modifier = Modifier.padding(20.dp))
     }
 
